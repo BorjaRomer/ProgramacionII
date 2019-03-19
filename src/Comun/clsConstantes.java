f659c2bf88cae92904ebe5b1cd82bd7cf6758a4f
Unknown <borja.romero@opendeusto.es>
2019-03-19 16:46:56 +0100

    clsConstantes

---
+++ src/Comun/clsConstantes.java
@@ -0,0 +1,28 @@
+package Comun;
+
+public class clsConstantes {
+	
+	public static final String PROPIEDAD_NUMBASTIDOR = "numbastidor";
+	public static final String PROPIEDAD_MARCA = "marca";
+	public static final String PROPIEDAD_MODELO = "modelo";
+	public static final String PROPIEDAD_CV = "cv";
+	public static final String PROPIEDAD_ANIOFABRICACION = "aniofabricacion";
+	public static final String PROPIEDAD_ESTADO = "estado";
+	public static final String PROPIEDAD_FECHA = "fecha";
+	public static final String PROPIEDAD_COLOR = "color";
+	public static final String PROPIEDAD_KILOMETROS = "kilometros";
+	
+	public static final String PROPIEDAD_COCHE_TIPOCOCHE = "tipocoche";
+	public static final String PROPIEDAD_COCHE_COMBUSTIBLE = "combustible";
+	public static final String PROPIEDAD_COCHE_CILINDRADA = "cilindrada";
+	
+	public static final String PROPIEDAD_MOTO_TIPOMOTO = "tipomoto";
+	public static final String PROPIEDAD_MOTO_CILINDRADA = "cilindrada";
+	public static final String PROPIEDAD_MOTO_TAMAÑO = "tamaño";
+	
+	public static final String PROPIEDAD_CAMION_TIPOCAMION = "tipocamion";
+	public static final String PROPIEDAD_CAMION_ALTURA = "altura";
+	public static final String PROPIEDAD_CAMION_ANCHURA = "anchura";
+	
+
+}
